56eb84ec4233845261a583209074f87355faf71e
Elliott Slaughter <slaughter@cs.stanford.edu>
2023-04-26 11:09:40 -0700

    OpenMPI modules.

---
+++ admin/modules/install_modules.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+set -e
+
+sudo apt update -qq
+sudo apt install -y environment-modules rsync
--- admin/modules/setup_modules.sh
+++ admin/modules/setup_modules.sh
@@ -2,9 +2,6 @@
 
 set -e
 
-sudo apt update -qq
-sudo apt install -y environment-modules rsync
-
 sudo mkdir -p /usr/local/modules
 sudo cp modulespath /usr/share/modules/init/.modulespath
 sudo rsync -rv --delete modules/ /usr/local/modules/
