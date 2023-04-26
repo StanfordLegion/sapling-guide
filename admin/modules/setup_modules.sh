63d3c24deb0e61c01d78e2a364fb168c1211648b
Elliott Slaughter <slaughter@cs.stanford.edu>
2023-04-25 22:30:17 -0700

    Sync modulefiles.

--- admin/modules/setup_modules.sh
+++ admin/modules/setup_modules.sh
@@ -3,7 +3,8 @@
 set -e
 
 sudo apt update -qq
-sudo apt install -y environment-modules
+sudo apt install -y environment-modules rsync
 
 sudo mkdir -p /usr/local/modules
 sudo cp modulespath /usr/share/modules/init/.modulespath
+sudo rsync -rv --delete modules/ /usr/local/modules/
