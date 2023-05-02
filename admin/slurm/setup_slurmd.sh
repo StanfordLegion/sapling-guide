e30c5e8cc400b6a31b179916d4b600e89460e2e4
Elliott Slaughter <slaughter@cs.stanford.edu>
2023-05-02 10:50:44 -0700

    And install all dependencies.

--- admin/slurm/setup_slurmd.sh
+++ admin/slurm/setup_slurmd.sh
@@ -2,6 +2,21 @@
 
 root_dir=$(readlink -f $(dirname "${BASH_SOURCE[0]}"))
 
+# install SLURM dependencies
+package_list=(
+    # hwloc
+    libxml2-dev libcairo2-dev libpciaccess-dev libudev-dev libltdl-dev
+
+    # pmix
+    libevent-dev
+
+    # SLURM
+    munge libmunge-dev # authentication
+)
+sudo apt update -qq
+sudo apt install -y "${package_list[@]}"
+
+# unpack SLURM installation
 pushd /
 if [[ ! -d /usr/local/hwloc-2.9.1 ]]; then
     sudo tar xfv $root_dir/install-hwloc-2.9.1.tar.gz
