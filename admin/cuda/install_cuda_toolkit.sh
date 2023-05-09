0906642a49912c89cbf01cfdad2217c8d508095b
Elliott Slaughter <slaughter@cs.stanford.edu>
2023-05-09 10:22:30 -0700

    Update CUDA installation to 12.1.

--- admin/cuda/install_cuda_toolkit.sh
+++ admin/cuda/install_cuda_toolkit.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-if [[ ! -f cuda_11.7.1_515.65.01_linux.run ]]; then
-    wget -nv https://developer.download.nvidia.com/compute/cuda/11.7.1/local_installers/cuda_11.7.1_515.65.01_linux.run
-    echo "52286a29706549b7d0feeb0e7e3eca1b15287c436a69fa880ad385b1be3e04db  cuda_11.7.1_515.65.01_linux.run" | shasum -c
+if [[ ! -f cuda_12.1.1_530.30.02_linux.run ]]; then
+    wget -nv https://developer.download.nvidia.com/compute/cuda/12.1.1/local_installers/cuda_12.1.1_530.30.02_linux.run
+    echo "d74022d41d80105319dfa21beea39b77a5b9919539c0487a05caaf2446d6a70e  cuda_12.1.1_530.30.02_linux.run" | shasum -c
 fi
-sudo sh cuda_11.7.1_515.65.01_linux.run --silent --toolkit
+sudo sh cuda_12.1.1_530.30.02_linux.run --silent --toolkit
--- admin/cuda/install_cudnn.sh
+++ admin/cuda/install_cudnn.sh
@@ -4,18 +4,19 @@ set -e
 
 # go to https://developer.nvidia.com/cudnn and follow instructions to download
 # (the link should be something like below, but you cannot access this without logging in)
-# https://developer.nvidia.com/compute/cudnn/secure/8.5.0/local_installers/11.7/cudnn-linux-x86_64-8.5.0.96_cuda11-archive.tar.xz
+# https://developer.nvidia.com/downloads/compute/cudnn/secure/8.9.1/local_installers/12.x/cudnn-linux-x86_64-8.9.1.23_cuda12-archive.tar.xz
 
-if [[ ! -f cudnn-linux-x86_64-8.5.0.96_cuda11-archive.tar.xz ]]; then
+if [[ ! -f cudnn-linux-x86_64-8.9.1.23_cuda12-archive.tar.xz ]]; then
     echo "Cannot locate cuDNN installer, did you download it?"
     exit 1
 fi
 
-if [[ ! -d cudnn-linux-x86_64-8.5.0.96_cuda11-archive ]]; then
-    tar xf cudnn-linux-x86_64-8.5.0.96_cuda11-archive.tar.xz
+if [[ ! -d cudnn-linux-x86_64-8.9.1.23_cuda12-archive ]]; then
+    echo "35163c5c542be0c511738b27e25235193cbeedc5e0e006e44b1cdeaf1922e83e  cudnn-linux-x86_64-8.9.1.23_cuda12-archive.tar.xz" | shasum -c
+    tar xf cudnn-linux-x86_64-8.9.1.23_cuda12-archive.tar.xz
 fi
 
 # https://docs.nvidia.com/deeplearning/cudnn/install-guide/index.html#installlinux-tar
-sudo cp cudnn-linux-x86_64-8.5.0.96_cuda11-archive/include/cudnn*.h /usr/local/cuda-11.7/include
-sudo cp -P cudnn-linux-x86_64-8.5.0.96_cuda11-archive/lib/libcudnn* /usr/local/cuda-11.7/lib64
-sudo chmod a+r /usr/local/cuda-11.7/include/cudnn*.h /usr/local/cuda-11.7/lib64/libcudnn*
+sudo cp cudnn-linux-x86_64-8.9.1.23_cuda12-archive/include/cudnn*.h /usr/local/cuda-12.1/include
+sudo cp -P cudnn-linux-x86_64-8.9.1.23_cuda12-archive/lib/libcudnn* /usr/local/cuda-12.1/lib64
+sudo chmod a+r /usr/local/cuda-12.1/include/cudnn*.h /usr/local/cuda-12.1/lib64/libcudnn*
