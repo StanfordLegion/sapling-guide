47e817f055cdb7cbacddd8bdfbee3e8488ec50dd
Elliott Slaughter <slaughter@cs.stanford.edu>
2023-06-01 14:32:49 -0700

    Container toolkit initial steps.

---
+++ admin/cuda/install_container_toolkit.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e
+
+# from https://docs.nvidia.com/datacenter/cloud-native/container-toolkit/install-guide.html#setting-up-nvidia-container-toolkit
+distribution=$(. /etc/os-release;echo $ID$VERSION_ID) \
+      && curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg \
+      && curl -s -L https://nvidia.github.io/libnvidia-container/$distribution/libnvidia-container.list | \
+            sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
+            sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
+
+sudo apt update -qq
