8080f5605a735c9a5929e282f80fbea4b4ade75b
Elliott Slaughter <slaughter@cs.stanford.edu>
2025-09-16 09:28:11 -0700

    Install CUDA 12.9.

--- admin/cuda/install_cuda_toolkit.sh
+++ admin/cuda/install_cuda_toolkit.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-if [[ ! -f cuda_12.1.1_530.30.02_linux.run ]]; then
-    wget -nv https://developer.download.nvidia.com/compute/cuda/12.1.1/local_installers/cuda_12.1.1_530.30.02_linux.run
-    echo "d74022d41d80105319dfa21beea39b77a5b9919539c0487a05caaf2446d6a70e  cuda_12.1.1_530.30.02_linux.run" | shasum -c
+if [[ ! -f cuda_12.9.1_575.57.08_linux.run ]]; then
+    wget -nv https://developer.download.nvidia.com/compute/cuda/12.9.1/local_installers/cuda_12.9.1_575.57.08_linux.run
+    echo "0f6d806ddd87230d2adbe8a6006a9d20144fdbda9de2d6acc677daa5d036417a  cuda_12.9.1_575.57.08_linux.run" | shasum -c
 fi
-sudo sh cuda_12.1.1_530.30.02_linux.run --silent --toolkit
+sudo sh cuda_12.9.1_575.57.08_linux.run --silent --toolkit
